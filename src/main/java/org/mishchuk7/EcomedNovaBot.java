c145c26f9c6a36a36df88908671c3dca73f4ecf3
smischenko <mishchenkoserhii7@gmail.com>
2022-09-02 14:10:17 +0300

    try to make text bold messageEntities.get(0).setType("bold");

--- src/main/java/org/mishchuk7/EcomedNovaBot.java
+++ src/main/java/org/mishchuk7/EcomedNovaBot.java
@@ -8,6 +8,7 @@ import org.mishchuk7.model.InternetDocument;
 import org.mishchuk7.model.InternetDocumentRequest;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -74,7 +75,8 @@ public class EcomedNovaBot extends TelegramLongPollingBot {
                         .chatId(chatId)
                         .text(result)
                         .build();
-                message.getEntities();
+                List<MessageEntity> messageEntities = message.getEntities();
+                messageEntities.get(0).setType("bold");
                 this.sendApiMethod(message);
             } catch (IOException | InterruptedException | TelegramApiException e) {
                 log.error("Exception when sending message: ", e);
