8e8a581c31f800eb1bba7ffc03f226b2b0523f45
Serhii <mishchenkoserhii7@gmail.com>
2022-09-03 22:33:17 +0300

    added method List<UserRequestHandler> getHandlers() and field Dispatcher dispatcher

--- src/main/java/org/mishchuk7/Application.java
+++ src/main/java/org/mishchuk7/Application.java
@@ -1,22 +1,43 @@
 package org.mishchuk7;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mishchuk7.hanlders.CallbackQueryHandler;
+import org.mishchuk7.hanlders.StartCommandHandler;
+import org.mishchuk7.hanlders.TextEnteredHandler;
+import org.mishchuk7.hanlders.UserRequestHandler;
+import org.mishchuk7.sender.EcomedNovaBotSender;
+import org.mishchuk7.service.TelegramService;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 public class Application {
+    private static final Dispatcher dispatcher = new Dispatcher(getHandlers());
+
     public static void main(String[] args) throws TelegramApiException {
 
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
             log.info("Registering bot...");
-            telegramBotsApi.registerBot(new EcomedNovaBot());
+            telegramBotsApi.registerBot(new EcomedNovaBot(dispatcher));
         } catch (TelegramApiRequestException e) {
             log.error("Failed to register bot(check internet connection / bot token or make sure only one instance of bot is running).", e);
         }
         log.info("Telegram bot is ready to accept updates from user......");
     }
+
+    private static List<UserRequestHandler> getHandlers() {
+        List<UserRequestHandler> handlers = new ArrayList<>();
+        TelegramService telegramService = new TelegramService(new EcomedNovaBotSender());
+        handlers.add(new StartCommandHandler(telegramService));
+        handlers.add(new CallbackQueryHandler(telegramService));
+        handlers.add(new TextEnteredHandler(telegramService));
+        return handlers;
+    }
+
 }
